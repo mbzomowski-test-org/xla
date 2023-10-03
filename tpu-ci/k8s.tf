7183c4e31de3023b66449a58e63dd235fbe40691
Mark Bzomowski <mbzomowski@gmail.com>
2023-10-03 21:09:01 +0000

    Fix kubernetes provider

--- tpu-ci/k8s.tf
+++ tpu-ci/k8s.tf
@@ -1,8 +1,9 @@
+data "google_client_config" "provider" {}
+
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-  client_key             = google_container_cluster.primary.master_auth.0.client_key
+  token = data.google_client_config.provider.access_token
 }
 
 resource "kubernetes_manifest" "flux-repo" {
