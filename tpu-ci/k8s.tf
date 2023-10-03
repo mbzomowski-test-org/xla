fa8a7d68453be3815b8ed5d85a96b7b9266aeee4
Mark Bzomowski <mbzomowski@gmail.com>
2023-10-03 20:45:59 +0000

    Fix kubernetes provider

--- tpu-ci/k8s.tf
+++ tpu-ci/k8s.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
   client_key             = google_container_cluster.primary.master_auth.0.client_key
 }
